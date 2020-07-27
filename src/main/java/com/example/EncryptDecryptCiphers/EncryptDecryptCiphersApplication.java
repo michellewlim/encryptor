1c7ab04722019ba832807515d79644de2d31564c
Michelle Lim <MichelleWLim@yahoo.com>
2020-07-27 16:12:11 -0700

    initial commit

---
+++ src/main/java/com/example/EncryptDecryptCiphers/EncryptDecryptCiphersApplication.java
@@ -0,0 +1,13 @@
+package com.example.EncryptDecryptCiphers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptDecryptCiphersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptDecryptCiphersApplication.class, args);
+	}
+
+}
